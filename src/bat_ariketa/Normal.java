fb14117a0dfefde25dc68a0e04f6f897cb38865a
spaidermart <martinamezola2000@gmail.com>
2021-05-03 19:28:01 +0200

    cosas

--- src/bat_ariketa/Normal.java
+++ src/bat_ariketa/Normal.java
@@ -5,8 +5,12 @@ public class Normala implements EgoeraModua {
 
     public Normala ()
     {
-
+        this.egin();
     }
 
+    public void egin()
+    {
+        System.out.printf("Alarma soinua jotzen ari da.");
+    }
 
 }
